e4e7e76ba9b08a1738ae08b0be94ba8b47b38d1f
gamzedilmac <32225824+gamzedilmac@users.noreply.github.com>
2019-11-24 22:03:15 +0300

    Angular's projects

--- site/src/app/services/product.service.ts
+++ site/src/app/services/product.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Product } from '../product/product';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators'
 
 @Injectable()
 export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts():Observable<Product[]> {
-   return this.http.get<Product[]>("http://localhost:3000/products")
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>("http://localhost:3000/products").pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    )
+  }
+  handleError(err: HttpErrorResponse) {
+    let errorMessage = ''
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = "Something went wrong " + err.error.message
+    } else {
+      errorMessage = "A systematic error is occured"
+    }
+    return throwError(errorMessage)
   }
 }
+
 
